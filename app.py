37c51648128b0db92b69a97e1953f5b1dcd05ccb
SahilGoyal9598 <70254054+SahilGoyal9598@users.noreply.github.com>
2023-05-13 08:23:42 +0000

    added dynamic data and API route

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+order_details = [{
+  'id': 1,
+  'unique_id': 123878548047,
+  'order no': '#200268987',
+  'sku': 'TP500BLUE-XXL',
+  'status': 'in cutting'
+}, {
+  'id': 2,
+  'unique_id': 123878548089,
+  'sku': 'DR500BLUE-M',
+  'status': 'in finishing'
+}, {
+  'id': 3,
+  'unique_id': 123878548090,
+  'order no': '#200268876',
+  'sku': 'KNTR121-BLAC-XXL',
+  'status': 'dispatched'
+}, {
+  'id': 4,
+  'unique_id': 123878548056,
+  'order no': '#200268845',
+  'sku': 'KNTR152GREY-L',
+  'status': 'Cancelled'
+}]
 
 
 # decorator in python "@"
 @app.route("/")  #home page
 def hello_world():
-  return "Hello world"
+  return render_template("home.html", orders=order_details)
+
+
+@app.route("/orders")  # orders page
+def orders():
+  return jsonify(order_details)
 
 
 print(__name__)
