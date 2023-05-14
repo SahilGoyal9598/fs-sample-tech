da1cc9c65397f9c14b2dc3859457735296f92621
SahilGoyal9598 <70254054+SahilGoyal9598@users.noreply.github.com>
2023-05-14 09:38:32 +0000

    added database and showing data from the database

--- app.py
+++ app.py
@@ -1,43 +1,26 @@
 from flask import Flask, render_template, jsonify
+from database import engine
+from sqlalchemy import text
 
 app = Flask(__name__)
 
-order_details = [{
-  'id': 1,
-  'unique_id': 123878548047,
-  'order no': '#200268987',
-  'sku': 'TP500BLUE-XXL',
-  'status': 'in cutting'
-}, {
-  'id': 2,
-  'unique_id': 123878548089,
-  'sku': 'DR500BLUE-M',
-  'status': 'in finishing'
-}, {
-  'id': 3,
-  'unique_id': 123878548090,
-  'order no': '#200268876',
-  'sku': 'KNTR121-BLAC-XXL',
-  'status': 'dispatched'
-}, {
-  'id': 4,
-  'unique_id': 123878548056,
-  'order no': '#200268845',
-  'sku': 'KNTR152GREY-L',
-  'status': 'Cancelled'
-}, {
-  'id': 5,
-  'unique_id': 123878548056,
-  'order no': '#200268845',
-  'sku': 'MGKS121BLUE-TAILORED',
-  'status': 'Cutting done'
-}]
+
+def load_orders_from_db():
+  with engine.connect() as conn:
+  result = conn.execute(text("select * from order_details"))
+
+  orders = []
+  for row in result.all():
+    orders.append(row._mapping)
+  return orders
 
 
 # decorator in python "@"
 @app.route("/")  #home page
 def hello_world():
-  return render_template("home.html", orders=order_details)
+  orders = load_orders_from_db()
+  
+  return render_template("home.html", orders=orders)
 
 
 @app.route("/orders")  # orders page
---
+++ database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+
+# this will connect to the database
+
+db_connection_string = "mysql+pymysql://07w6gigu86t6fo0rnime:pscale_pw_Bx5P7Bi34K8BTDjkMDOrfBxczgueZmzkXNEz9SiQ2tF@aws.connect.psdb.cloud/fs-sample-tech?charset=utf8mb4"
+
+engine = create_engine(db_connection_string,
+                       connect_args={
+                         "ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+
+
