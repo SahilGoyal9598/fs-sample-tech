a6d371d2183143cd82adab65e6d595350b8382fc
SahilGoyal9598 <70254054+SahilGoyal9598@users.noreply.github.com>
2023-05-22 14:30:49 +0000

    added orders templates and added functionality to add new orders to the database

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, jsonify
-from database import load_orders_from_db, load_order_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_orders_from_db, load_order_from_db, add_cancel_to_db, add_scan_unique_to_db, add_new_orders_to_db, order_tracking_filter, all_orders
+import csv
+from datetime import datetime
+#import pandas as pd
 
 app = Flask(__name__)
 
@@ -18,6 +21,68 @@ def show_order(id):
   if not orders:
     return "Not found", 404
   return render_template("single_order.html", orders=orders)
+
+
+@app.route("/cancel/<id>/apply", methods=['post'])
+def apply_to_order(id):
+  data = request.form
+  add_cancel_to_db(id, data)
+
+  return jsonify(data)
+  # store to th Database
+  # send an confirmation email
+  # Display the acknoledgement
+
+
+@app.route("/scan", methods=['GET'])
+def scan_page():
+  return render_template("scan_page.html")
+
+
+@app.route("/scan_order", methods=['POST'])
+def scan_order_in_production():
+  scan_id = request.form['unique_id_scan']
+  scan_status = request.form['scan_status']
+  add_scan_unique_to_db(scan_id, scan_status)
+
+  return render_template("scan_page.html")
+
+
+@app.route("/new_orders", methods=['GET'])
+def show_new_order_page():
+  return render_template("fresh_orders.html")
+
+
+@app.route("/upload", methods=['POST'])
+def add_new_order():
+  csvFile = request.files['csvFile']
+  csv_data = csvFile.read().decode('utf-8').splitlines()
+  csv_reader = csv.reader(csv_data)
+  next(csv_reader)  # skip header row if needed
+
+  for row in csv_reader:
+    unique_id = row[0]
+    order_no = row[1]
+    order_status = row[2]
+    orderDate = row[3]
+    tmpDate = orderDate.split('-')
+    dateString = tmpDate[0] + '/' + tmpDate[1] + '/' + tmpDate[2][2:4:1]
+    order_date = datetime.strptime(dateString, '%d/%m/%y').date()
+    # print('ola')
+    # print(order_date)
+    # print('ola')
+    # order_date = datetime.strptime(order_date, "%d-%m-%y").strftime("%y-%m-%d")
+
+    add_new_orders_to_db(unique_id, order_no, order_status, order_date)
+  return render_template("fresh_orders.html")
+
+  return "Invalid CSV file"
+
+
+@app.route("/all_orders")
+def show_all_orders():
+  all_order = all_orders()
+  return render_template("all_orders.html", all_order=all_order)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -32,3 +32,54 @@ def load_order_from_db(id):
       return None
     else:
       return [dict(row) for row in rows]
+
+
+def add_cancel_to_db(id, data):
+  with engine.connect() as conn:
+
+    query = text(
+      f"insert into order_status_log (order_id, unique_id) values ('{id}','{data}')"
+    )
+
+    conn.execute(query)
+
+
+def add_scan_unique_to_db(scan_id, scan_status):
+  with engine.connect() as conn:
+
+    query = text(
+      f"insert into order_status_log (unique_id, order_status) values ('{scan_id}','{scan_status}')"
+    )
+    conn.execute(query)
+
+
+def add_new_orders_to_db(scan_id, order, status, date):
+  with engine.connect() as conn:
+
+    query = text(
+      f"insert into orders (unique_id,order_no,order_status,order_date) values ('{scan_id}','{order}','{status}','{date}')"
+    )
+    conn.execute(query)
+
+
+def order_tracking_filter():
+  with engine.connect() as conn:
+
+    query = text(
+      f"select distinct(order_status) from orders order by order_status ")
+
+  order_status = []
+  for row in query.all():
+    order_status.append(row._mapping)
+  return order_status
+
+
+def all_orders():
+  with engine.connect() as conn:
+    query = text("select * from orders limit 10")
+    result = conn.execute(query)
+
+    all_orders_data = []
+    for row in result.all():
+      all_orders_data.append(row._mapping)
+    return all_orders_data
